be225d53691897bd78dd839de7935fcf94a2a995
ilov3gam3z <ilov3gam3z@gmail.com>
2024-09-10 15:54:11 -0400

    Spearhead Beta 1.4
    Added error codes.

--- Spearhead/Terminal.py
+++ Spearhead/Terminal.py
@@ -3,11 +3,18 @@ import sys
 import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 dir_path = dir_path + "\\Y_frontend.py"
+failedAsError = False
 def run_c(c):
     try:
         #start terminal and get output
         output = subprocess.check_output(c, shell=True, stderr=subprocess.DEVNULL).decode()
-        return output.decode()
+        if output == '':
+            print("Err0x1: Command NULL does not exist")
+            global failedAsError
+            failedAsError = True
+            return ''
+        if failedAsError != True:
+            return output.decode()
     except subprocess.CalledProcessError as e:
         #incase i need to handle errors later
         return e.output.decode()
@@ -23,18 +30,18 @@ def main():
             print("Terminal instructions:")
             print("Press the enter key or enter \"exit\" into the command line to leave the terminal")
             print("Enter the command \"+r [replace with path of .spearhead file to be ran]\" to run a .spearhead file via the Spearhead Interpreter")
-        #get user input and check if it is "+r"
+        #get user input and check if it is "+r" 
         elif u_i[:2] == "+r":
             try:
                 r = subprocess.run(["python", dir_path, u_i], capture_output=True, text=True).stdout.strip("\n")
                 print(r)
                 if r == '':
-                    print("Directory invalid")
+                    print("Err1x1: Directory invalid")
             except FileNotFoundError as e:
-                print("Directory invalid")
+                print("Err1x1: Directory invalid")
             continue
         else:
-            print("Invalid command")
+            print("Err0x2:Invalid command")
         #actually run the commands provided and print output
         output = run_c(u_i)
         print(output)
